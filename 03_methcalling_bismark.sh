f4f68f4a8fdd84feada06988a9f3253a190fae91
Veronika Laine <nvlain@users.noreply.github.com>
2023-05-25 11:11:28 +0300

    Update 03_methcalling_bismark.sh

--- 03_methcalling_bismark.sh
+++ 03_methcalling_bismark.sh
@@ -1,4 +1,4 @@
 ##bash
 for f in ./02_alignment/*.bam; do
-  bismark_methylation_extractor -p --no_overlap --report --bedGraph --scaffolds --cytosine_report --ignore 3 --ignore_r2 2 --ignore_3prime_r2 1 --genome_folder /mnt/nfs/bioinfdata/home/NIOO/veronikal/projects/methylation_arsenic/genome "$f" 
+  bismark_methylation_extractor -p --no_overlap --report --bedGraph --scaffolds --cytosine_report --ignore 3 --ignore_r2 2 --ignore_3prime_r2 1 --genome_folder /projects/methylation_arsenic/genome "$f" 
 done
